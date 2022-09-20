2565f5d9ff8b7c6b9a9edf4ba2c308c688004969
jianglong <jianglong199212@gmail.com>
2022-09-20 10:42:35 +0800

    最小数组

---
+++ coding_interviews/alag/search/find_nth_digit.go
@@ -0,0 +1,32 @@
+package search
+
+import (
+	"strconv"
+)
+
+/**
+* 代码中的类名、方法名、参数名已经指定，请勿修改，直接返回方法规定的值即可
+*
+*
+* @param n int整型
+* @return int整型
+01234567891011121314
+* [0,10) [10,100), [100,1000) [1000,10000)
+*/
+func findNthDigit(n int) int {
+	if n < 10 {
+		return n
+	}
+	i := 1
+	a, b := 0, 10
+	for n > (b-a)*i {
+		n -= (b - a) * i
+		i++
+		a, b = b, b*10
+	}
+
+	num := n/i + a
+	index := n % i
+	x := strconv.Itoa(num)
+	return int([]byte(x)[index] - '0')
+}
---
+++ coding_interviews/alag/search/find_nth_digit_test.go
@@ -0,0 +1,50 @@
+package search
+
+import "testing"
+
+func Test_findNthDigit(t *testing.T) {
+	type args struct {
+		n int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				n: 0,
+			},
+			want: 0,
+		},
+		{
+			name: "test2",
+			args: args{
+				n: 2,
+			},
+			want: 2,
+		},
+		{
+			name: "test3",
+			args: args{
+				n: 10,
+			},
+			want: 1,
+		},
+		{
+			name: "test4",
+			args: args{
+				n: 13,
+			},
+			want: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := findNthDigit(tt.args.n); got != tt.want {
+				t.Errorf("findNthDigit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
