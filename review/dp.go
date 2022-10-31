68c369a3d48d90b8cd268e3ddd8327b909b953f5
jianglong <jianglong199212@gmail.com>
2022-10-31 17:25:35 +0800

    upadte

--- review/dp.go
+++ review/dp.go
@@ -1,6 +1,8 @@
 package review
 
-import "math"
+import (
+	"math"
+)
 
 func FindGreatestSumOfSubArray(array []int) int {
 	ret := math.MinInt64
@@ -17,5 +19,156 @@ func FindGreatestSumOfSubArray(array []int) int {
 	return ret
 }
 
-func FindGreatestSumOfSubArray2(array []int) int {
+// 回溯法
+// 选择范围+有效选择+回退
+func MinSumPath(paths [][]int) int {
+	min := math.MaxInt64
+
+	var tf func(i, s int, path []int)
+	tf = func(i, s int, path []int) {
+		if len(path) == len(paths) { // 计算最小值
+			tmp := 0
+			for _, v := range path {
+				tmp += v
+			}
+			if tmp < min {
+				min = tmp
+			}
+
+			return
+		}
+
+		selects := make([]int, 0)
+		if i == 0 {
+			for x := 0; x < len(paths[i]); x++ {
+				selects = append(selects, x)
+			}
+		} else {
+			// 左下
+			if s-1 >= 0 {
+				selects = append(selects, s-1)
+			}
+			// 正下
+			selects = append(selects, s)
+
+			// 右下
+			if s+1 < len(paths[i]) {
+				selects = append(selects, s+1)
+			}
+		}
+
+		for _, item := range selects {
+			path = append(path, paths[i][item])
+			tf(i+1, item, path)
+			path = path[0 : len(path)-1]
+		}
+	}
+
+	tf(0, 0, nil)
+	return min
+}
+
+func min(args ...int) int {
+	if len(args) == 1 {
+		return args[0]
+	}
+
+	a := args[0]
+	b := min(args[1:]...)
+	if a > b {
+		return b
+	}
+
+	return a
+}
+
+// 动态规划
+func MinFallingPathSum(matrix [][]int) int {
+	if len(matrix) == 0 {
+		return 0
+	}
+
+	res := math.MaxInt64
+	var dp func(i, j int) int
+	dp = func(i, j int) int {
+		if i < 0 || j > len(matrix)-1 || j < 0 {
+			return math.MaxInt64
+		}
+
+		if i == 0 {
+			return matrix[0][j]
+		}
+
+		return matrix[i][j] + min(dp(i-1, j), dp(i-1, j-1), dp(i-1, j+1))
+	}
+
+	l := len(matrix)
+	for i := 0; i < len(matrix[0]); i++ {
+		res = min(res, min(dp(l-1, i-1), dp(l-1, i), dp(l-1, i+1)))
+	}
+
+	return res
+}
+
+func max(args ...int) int {
+	if len(args) == 1 {
+		return args[0]
+	}
+
+	tmp := max(args[1:]...)
+	if tmp > args[0] {
+		return tmp
+	}
+
+	return args[0]
+}
+
+func maxValue(grid [][]int) int {
+	if len(grid) == 0 {
+		return 0
+	}
+	if len(grid) == 1 {
+		tmp := 0
+		for i := 0; i < len(grid[0]); i++ {
+			tmp += grid[0][i]
+		}
+
+		return tmp
+	}
+
+	var vars [200][200]*int
+
+	// write code here
+	var dp func(i, j int) int
+	dp = func(i, j int) int {
+		if vars[i][j] != nil {
+			return *vars[i][j]
+		}
+
+		if i == 0 {
+			tmp := 0
+			for j >= 0 {
+				tmp += grid[0][j]
+				j--
+			}
+			return tmp
+		}
+
+		if j == 0 {
+			tmp := 0
+			for i >= 0 {
+				tmp += grid[i][0]
+				i--
+			}
+
+			return tmp
+		}
+
+		t := grid[i][j] + max(dp(i-1, j), dp(i, j-1))
+		vars[i][j] = &t
+
+		return t
+	}
+
+	return dp(len(grid)-1, len(grid[0])-1)
 }
--- review/dp_test.go
+++ review/dp_test.go
@@ -1,6 +1,8 @@
 package review
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFindGreatestSumOfSubArray(t *testing.T) {
 	type args struct {
@@ -24,6 +26,137 @@ func TestFindGreatestSumOfSubArray(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := FindGreatestSumOfSubArray(tt.args.array); got != tt.want {
 				t.Errorf("FindGreatestSumOfSubArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMinSumPath(t *testing.T) {
+	type args struct {
+		paths [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				paths: [][]int{
+					{1, 2, 3},
+					{4, 5, 6},
+					{7, 8, 9},
+				},
+			},
+			want: 12,
+		},
+		{
+			name: "test2",
+			args: args{
+				paths: [][]int{
+					{2, 1, 3},
+					{6, 5, 4},
+					{7, 8, 9},
+				},
+			},
+			want: 13,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MinSumPath(tt.args.paths); got != tt.want {
+				t.Errorf("MinSumPath() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMinFallingPathSum(t *testing.T) {
+	type args struct {
+		matrix [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				matrix: [][]int{
+					{1, 2, 3},
+					{4, 5, 6},
+					{7, 8, 9},
+				},
+			},
+			want: 12,
+		},
+		{
+			name: "test2",
+			args: args{
+				matrix: [][]int{
+					{2, 1, 3},
+					{6, 5, 4},
+					{7, 8, 9},
+				},
+			},
+			want: 13,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MinFallingPathSum(tt.args.matrix); got != tt.want {
+				t.Errorf("MinFallingPathSum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxValue(t *testing.T) {
+	type args struct {
+		grid [][]int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				grid: [][]int{
+					{1, 3, 1},
+					{1, 5, 1},
+					{4, 2, 1},
+				},
+			},
+			want: 12,
+		},
+		{
+			name: "test2",
+			args: args{
+				grid: [][]int{
+					{9, 1, 4, 8},
+				},
+			},
+			want: 22,
+		},
+		{
+			name: "test3",
+			args: args{
+				grid: [][]int{
+					{1, 2, 5},
+					{3, 2, 1},
+				},
+			},
+			want: 9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxValue(tt.args.grid); got != tt.want {
+				t.Errorf("maxValue() = %v, want %v", got, tt.want)
 			}
 		})
 	}
