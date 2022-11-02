69a72d22944608a8a94cdbf63f764a185dbcb64a
jianglong <jianglong199212@gmail.com>
2022-11-02 14:03:12 +0800

    update

--- coding_interviews/dp/bag.go
+++ coding_interviews/dp/bag.go
@@ -10,6 +10,7 @@ type bagv struct {
 	v int
 }
 
+// 0 1背包
 func bag(m []*bagv, size int) int {
 	dp := make([][]int, len(m)+1)
 
@@ -21,6 +22,27 @@ func bag(m []*bagv, size int) int {
 		for j := 1; j <= size; j++ {
 			if j >= m[i-1].w {
 				dp[i][j] = common.Max(dp[i-1][j], dp[i-1][j-m[i-1].w]+m[i-1].v)
+			} else {
+				dp[i][j] = dp[i-1][j]
+			}
+		}
+	}
+
+	return dp[len(m)][size]
+}
+
+// 完全背包
+func bag2(m []*bagv, size int) int {
+	dp := make([][]int, len(m)+1)
+
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, size+1)
+	}
+
+	for i := 1; i <= len(m); i++ {
+		for j := 1; j <= size; j++ {
+			if j >= m[i-1].w {
+				dp[i][j] = common.Max(dp[i-1][j], dp[i][j-m[i-1].w]+m[i-1].v)
 			} else {
 				dp[i][j] = dp[i-1][j]
 			}
--- coding_interviews/dp/bag_test.go
+++ coding_interviews/dp/bag_test.go
@@ -1,6 +1,8 @@
 package dp
 
-import "testing"
+import (
+	"testing"
+)
 
 func Test_bag(t *testing.T) {
 	type args struct {
@@ -38,6 +40,47 @@ func Test_bag(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := bag(tt.args.m, tt.args.size); got != tt.want {
 				t.Errorf("bag() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_bag2(t *testing.T) {
+	type args struct {
+		m    []*bagv
+		size int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test1",
+			args: args{
+				m: []*bagv{
+					{
+						w: 1,
+						v: 1500,
+					},
+					{
+						w: 3,
+						v: 2000,
+					},
+					{
+						w: 4,
+						v: 3000,
+					},
+				},
+				size: 4,
+			},
+			want: 6000,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := bag2(tt.args.m, tt.args.size); got != tt.want {
+				t.Errorf("bag2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
