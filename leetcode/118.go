c0b96a5932e681389c68b7de462c669eb7416b50
jianglong <jianglong199212@gmail.com>
2023-02-15 15:23:46 +0800

    leetcode118一次遍历.

--- leetcode/118.go
+++ leetcode/118.go
@@ -3,15 +3,20 @@ package leetcode
 // dp(i,j) = 1 // j ==0 or j == row - 1
 // dp(i,j) = dp(i-1, j-1) + dp(i -1, j)
 // 只能自底向上，不能自顶向下
+// 严格来说不是动态规划，没有求最值。
 func generate(numRows int) [][]int {
 	ans := make([][]int, numRows)
 	for i := 0; i < numRows; i++ {
-		ans[i] = make([]int, i+1)
-	}
+		if i == 0 {
+			ans[i] = []int{1}
+			continue
+		}
+		if i == 1 {
+			ans[i] = []int{1, 1}
+			continue
+		}
 
-	ans[0] = []int{1}
-	ans[1] = []int{1, 1}
-	for i := 2; i < numRows; i++ {
+		ans[i] = make([]int, i+1)
 		for j := 0; j <= i; j++ {
 			if j == 0 || j == i {
 				ans[i][j] = 1
@@ -21,5 +26,5 @@ func generate(numRows int) [][]int {
 		}
 	}
 
-	return ans[0:numRows]
+	return ans
 }
